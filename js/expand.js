2bd8ccb6d1c613bb64ac5a644ff605d4f281e278
lezzthanthree <arisatominato48@gmail.com>
2024-02-25 11:12:45 +0800

    Added edits section

--- js/expand.js
+++ js/expand.js
@@ -1,5 +1,18 @@
 var current_section = ''
 
+var videoLinks = 
+{
+    "introMonika" : "https://www.youtube.com/embed/YONRwNL1C3E?si=D81sM4Mvxf8TuoQt",
+    "YOHAQUEST": "https://www.youtube.com/embed/3FCkCSVESY0?si=EH5bcpBwrxr5GL5R",
+    "karinMilk" : "https://www.youtube.com/embed/SQl7fg_QSQM?si=qYnfhRtXFv98qYz1",
+    "kasumiBurger": "https://www.youtube.com/embed/B0xk99GcOAk?si=yXqPqMC46Q5pCLWE",
+    "noDokiKnows": "https://www.youtube.com/embed/Hi9I4KD-uT0?si=_-CtpTD72BW1ju8o",
+    "hey": "https://www.youtube.com/embed/0qTNnB7WES0?si=RpcsInhjo80Tgsk3",
+    "animePlot": "https://www.youtube.com/embed/Uh6vl2l-E3o?si=M6b4Fuw4UFyPNvbX",
+    "ligma": "https://www.youtube.com/embed/odaV9n9uJhA?si=XQsQEbpcW_fOB54D",
+    "setsunaTricycle": "https://www.youtube.com/embed/jGyeH5ilvEU?si=pysk8eortanqjIjT"
+}
+
 function expand(section)
 {
     current_section = section
@@ -7,9 +20,19 @@ function expand(section)
         $(".page").fadeIn(300);
         $("#" + section).fadeIn(300);
     });
+
+    if (section == 'edits')
+    {
+        $("#edit-preview").fadeIn(300)
+    }
 }
 
 function reset()
 {
     $(".page, #" + current_section).fadeOut(300, () => $("#main").fadeIn(300))
+}
+
+function showpreview(url)
+{
+    $("#video-preview").attr('src', videoLinks[url])
 }
